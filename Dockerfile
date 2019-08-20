46544e85ab893633b9a63ca6039d22b9ca973568
immerrr <immerrr@gmail.com>
2019-08-20 11:17:43 +0300

    Add TCP_OUTGOING_ADDRESS envvar for the docker image

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM alpine:3.8
 
 LABEL maintainer="denis@localistico.com"
 
+ENV TCP_OUTGOING_ADDRESS
+
 RUN apk update && \
     apk add squid curl && \
     echo "username:$(echo password | mkpasswd -m sha512)" > /etc/squid/htpasswd && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,10 @@ mkdir -p /var/log/squid && \
 # Set up access log redirection to stdout.
 chmod o+w /dev/stdout && ln -sf /dev/stdout /var/log/squid/access.log
 
+test -z "$TCP_OUTGOING_ADDRESS" || {
+    echo "tcp_outgoing_address $TCP_OUTGOING_ADDRESS" > /etc/squid/squid.conf.d/99-outgoing-address
+}
+
 if test $# -eq 0; then
     # No command line: run squid
     exec squid -NYC -d 1 -f /etc/squid/squid.conf
